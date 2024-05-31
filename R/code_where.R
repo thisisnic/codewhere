7cfe64e5b388f8f0104fe9736967476bd9bb2de7
Nic Crane <thisisnic@gmail.com>
2024-05-31 09:39:32 +0100

    Add example and return type

--- R/code_where.R
+++ R/code_where.R
@@ -3,20 +3,23 @@
 #' @param package_name The name of the package
 #' @param package_type One of "cran", "bioc", or "github"
 #' @export
+#' @return Path to where code can be found or NULL
+#' @examples
+#' \dontrun{
+#' code_where("dplyr")
+#' }
 code_where <- function(package_name, package_type = "cran") {
-
   match.arg(package_type, c("cran", "bioc", "github"))
 
-  if(package_type == "cran") {
+  if (package_type == "cran") {
     url <- extract_url_cran(package_name)
-  } else if(package_type == "bioc") {
+  } else if (package_type == "bioc") {
     url <- extract_bioc_url(package_name)
-  } else if(package_type == "github") {
+  } else if (package_type == "github") {
     url <- extract_github_url(package_name)
   }
 
-  if(test_url(url)){
+  if (test_url(url)) {
     return(url)
   }
-
 }
